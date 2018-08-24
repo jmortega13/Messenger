a04ba916d8d4cc530277fd154aa2cd2a253d2935
Manuel Ortega Jr <manuel.ortega@synacy.com>
2018-08-24 18:03:08 +0800

    Add interface ' Messenger '

---
+++ exercise-remove-conditionals/src/com/synacy/exercise/Messenger.java
@@ -0,0 +1,13 @@
+package com.synacy.exercise;
+
+
+/**
+ * Refactor so you don't have to have an if statement here. You can change/create anything.
+ * Only requirement is the functionality is retained.
+ *
+ * */
+public interface Messenger {
+
+	void sendMessage(String message, Contact contact);
+
+}
